6fe70929784dd993aa66dbecfad6254e3717c6b8
allanstar-byte <allanmutisya263@gmail.com>
2022-06-04 09:44:40 +0300

    Events

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 function addingEventListener() {
+    alert("'I was clicked!'")
+    const input_2 = document.getElementById("input")
+    input_2.addEventListener("click", addingEventListener)
 }
+
+// const input_2 = document.getElementById("input")
+
+// input_2.addEventListener("click", addingEventListener)
+
+let input_1 = document.getElementById('input');
+input_1.addEventListener('click', function() {
+    alert('I was clicked!');
+});
+
+let input = document.getElementById("input")
+
+function lisner() {
+    // if (input != String)
+    alert("Kiss me Bono !")
+}
+
+input.addEventListener("mouseleave", lisner)
